f016ab4c85d3c7651034dadd3628ea59d57a66a2
paulos <paulo@3mw.com.br>
2018-03-12 08:38:07 -0300

    progress

--- servicedesk/admin.py
+++ servicedesk/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from servicedesk.models import Request, Queue, Update
+
+
+class UpdateInline(admin.StackedInline):
+    model = Update
+    extra = 1
+    can_delete = False
+
+    def get_queryset(self, request):
+        return self.model.objects.none()
+
+
+class RequestAdmin(admin.ModelAdmin):
+    raw_id_fields = ['responsible']
+    readonly_fields = ['created_by', 'created', 'updated']
+    inlines = [UpdateInline]
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        if extra_context is None:
+            extra_context = {}
+        extra_context['update_list'] = Update.objects.filter(request=object_id)
+        return super().change_view(request, object_id, form_url, extra_context)
+
+
+admin.site.site_header = 'Encantador'
+admin.site.register(Request, RequestAdmin)
+admin.site.register(Queue)
+
--- servicedesk/models.py
+++ servicedesk/models.py
@@ -12,16 +12,25 @@ class Queue(models.Model):
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.name
+
 
 class Request(models.Model):
+    STATUS = (
+        ('new', 'New'),
+        ('open', 'Open'),
+        ('closed', 'Closed'),
+        ('removed', 'Removed'),
+    )
     queue = models.ForeignKey(Queue, on_delete=models.PROTECT)
     created_by = models.EmailField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     subject = models.CharField(max_length=200)
     requester = models.EmailField()
-    responsible = models.EmailField()
-    status = FSMField(default='new')
+    responsible = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = FSMField(default='new', choices=STATUS)
 
     @transition(field=status, source='new', target='open')
     def assign(self, responsible):
@@ -36,7 +45,7 @@ class Request(models.Model):
         self.record_comment(author, message, comment_type, attachments)
 
     def record_comment(self, author, message, comment_type, attachments):
-        if comment_type == RESPONSE and author not in (self.requester, self.responsible):
+        if comment_type == RESPONSE and author not in (self.requester, self.responsible.email):
             try:
                 user = User.objects.get(email=author)
             except User.DoesNotExist:
@@ -45,14 +54,17 @@ class Request(models.Model):
         if attachments:
             for payload in attachments:
                 Attachment.objects.create(request=self, payload=payload)
-        Comment.objects.create(request=self, author=author, message=message, type=comment_type)
+        Update.objects.create(request=self, author=author, message=message, type=comment_type)
 
     @transition(field=status, source='*', target='closed')
     def close(self, author, message, comment_type=RESPONSE, attachments=None):
         self.record_comment(author, message, comment_type, attachments)
 
+    def __str__(self):
+        return "[{} #{}] {}".format(self.queue.name, self.pk, self.subject)
 
-class Comment(models.Model):
+
+class Update(models.Model):
     TYPE = (
         (COMMENT, 'Comment'),
         (RESPONSE, 'Response'),
@@ -63,8 +75,14 @@ class Comment(models.Model):
     type = models.IntegerField(choices=TYPE, default=0)
     message = models.TextField()
 
+    def __str__(self):
+        return self.get_type_display()
+
+    class Meta:
+        verbose_name = 'Update'
+
 
 class Attachment(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Update, on_delete=models.CASCADE)
     payload = models.FileField(upload_to='attachments')
     mime_type = models.CharField(max_length=100)
